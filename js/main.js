d64c092b50c90f100a309baec105f42d03a4dbfe
2bohany <youssef2000649@gmail.com>
2025-04-08 14:49:05 +0200

    git init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const productList = [
+    {
+      id: 'p1',
+      name: 'Wireless Headphones',
+      price: 299.00,
+      image: 'assets/images/product1.jpg'
+    },
+    {
+      id: 'p2',
+      name: 'Gaming Mouse',
+      price: 149.00,
+      image: 'assets/images/product2.jpg'
+    },
+    {
+      id: 'p3',
+      name: '4K Monitor',
+      price: 899.00,
+      image: 'assets/images/product3.jpg'
+    },
+    // Add more as needed
+  ];
+  
